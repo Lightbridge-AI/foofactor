b7ee4fb1f74fdf554e45d53b503c639aa8356ea1
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-01-15 20:09:39 +0700

    Add DESCIPTION, LICENSE, Manual

--- R/fbind.R
+++ R/fbind.R
@@ -1,9 +1,15 @@
-
-# fbind : Concatenate 2 factors -------------------------------------------
-
-
-
-
+#' Bind two factors
+#'
+#' create new factor from two existing factors
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
--- R/simsum.R
+++ R/simsum.R
@@ -1,3 +1,12 @@
-## Silly sum
-
+#' Silly sum
+#'
+#' sum() with na.rm = TRUE
+#'
+#' @param ... numeric
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' simsum(rep(c(NA,1:10),2))
 simsum <- function(...){ sum(..., na.rm = T)}
